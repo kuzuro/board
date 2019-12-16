47ef804787d7a5d69dd9201b6dd1ef022dc21585
현우 이 <kuzuro@gmail.com>
2019-12-16 21:55:11 +0900

    #commit 10
    페이징 기능 추가

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -95,11 +95,44 @@ public class BoardController {
 		
 		// 출력할 게시물
 		int displayPost = (num - 1) * postNum;
-				
+					
+		
+		// 한번에 표시할 페이징 번호의 갯수
+		int pageNum_cnt = 10;
+		
+		// 표시되는 페이지 번호 중 마지막 번호
+		int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
+		
+		// 표시되는 페이지 번호 중 첫번째 번호
+		int startPageNum = endPageNum - (pageNum_cnt - 1);
+		
+		// 마지막 번호 재계산
+		int endPageNum_tmp = (int)(Math.ceil((double)pageNum / (double)pageNum_cnt));
+		
+		if(endPageNum > endPageNum_tmp) {
+			endPageNum = endPageNum_tmp;
+		}
+		
+		boolean prev = startPageNum == 1 ? false : true;
+		boolean next = endPageNum * pageNum_cnt >= pageNum ? false : true; 
+		
+		
 		List<BoardVO> list = null; 
 		list = service.listPage(displayPost, postNum);
 		model.addAttribute("list", list);		 
 		model.addAttribute("pageNum", pageNum);
+		
+		// 시작 및 끝 번호
+		model.addAttribute("startPageNum", startPageNum);
+		model.addAttribute("endPageNum", endPageNum);
+		
+		// 이전 및 다음 
+		model.addAttribute("prev", prev);
+		model.addAttribute("next", next);
+		
+		// 현재 페이지
+		model.addAttribute("select", num);
+		
 	}
 }
-
+ 
--- board/src/main/webapp/WEB-INF/views/board/listPage.jsp
+++ board/src/main/webapp/WEB-INF/views/board/listPage.jsp
@@ -47,11 +47,35 @@
 </table>
 
 <div>
-	<c:forEach begin="1" end="${pageNum}" var="num">
+	
+	<c:if test="${prev}">
+		<span>[ <a href="/board/listPage?num=${startPageNum - 1}">이전</a> ]</span>
+	</c:if>
+	
+	<c:forEach begin="${startPageNum}" end="${endPageNum}" var="num">
+  		<span>
+  		
+  			<c:if test="${select != num}">
+  				<a href="/board/listPage?num=${num}">${num}</a>
+  			</c:if> 			
+  			
+  			<c:if test="${select == num}">
+  				<b>${num}</b>
+  			</c:if>
+  			
+		</span>
+	</c:forEach>
+	
+	<c:if test="${next}">
+		<span>[ <a href="/board/listPage?num=${endPageNum + 1}">다음</a> ]</span>
+	</c:if>
+	
+	
+	<%-- <c:forEach begin="1" end="${pageNum}" var="num">
   		<span>
   			<a href="/board/listPage?num=${num}">${num}</a>
 		</span>
-	</c:forEach>
+	</c:forEach> --%>
 </div>
 
 
