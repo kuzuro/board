277b04d3f43eb0b367174e5ac733279c974bcb1a
현우 이 <kuzuro@gmail.com>
2020-11-04 02:36:42 +0900

    #commit 15
    댓글 기능 구현 2

--- board/src/main/java/com/board/controller/ReplyController.java
+++ board/src/main/java/com/board/controller/ReplyController.java
@@ -4,7 +4,9 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.board.domain.ReplyVO;
 import com.board.service.ReplyService;
 
 @Controller
@@ -17,6 +19,13 @@ public class ReplyController {
 	// 댓글 조회
 	
 	// 댓글 작성
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String posttWirte(ReplyVO vo) throws Exception {
+		
+		replyService.write(vo);
+		
+		return "redirect:/board/view?bno=" + vo.getBno();
+	}
 	
 	// 댓글 수정
 	
--- board/src/main/webapp/WEB-INF/views/board/view.jsp
+++ board/src/main/webapp/WEB-INF/views/board/view.jsp
@@ -81,15 +81,21 @@ ${view.content}<br />
 </ul>
 
 <div>
-	<p>
-		<label>댓글 작성자</label> <input type="text">
-	</p>
-	<p>
-		<textarea rows="5" cols="50"></textarea>
-	</p>
-	<p>
-		<button type="button">댓글 작성</button>
-	</p>
+
+	<form method="post" action="/reply/write">
+	
+		<p>
+			<label>댓글 작성자</label> <input type="text" name="writer">
+		</p>
+		<p>
+			<textarea rows="5" cols="50" name="content"></textarea>
+		</p>
+		<p>
+			<input type="hidden" name="bno" value="${view.bno}">
+			<button type="submit">댓글 작성</button>
+		</p>
+	</form>
+	
 </div>
 
 <!-- 댓글 끝 -->
